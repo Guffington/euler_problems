40d188b2bfb6b26732ab99e01b3f5ecae72184c0
Tom Guff <tom.guff@mq.edu.au>
2025-08-15 11:35:56 +1000

    Properly commented

--- problem_1.py
+++ problem_1.py
@@ -1,10 +1,17 @@
 def problem_one(n):
+    """
+    Returns the sum of all multiples of 3 or 5 below n.
+    
+    """
     counter = 0
     for i in range(n):
+        # If the number is a multiple of 3, add it to the counter
         if i % 3 == 0:
             counter += i
+        # If the number is not a multiple of 3 but is a multiple of 5, add it to the counter
         elif i % 5 == 0:
             counter += i
     return counter
-        
+
+# Euler problem 1: Sum of all multiples of 3 and 5 for all natural numbers below 1000 
 print(problem_one(1000))
